91f1a3eb6753c4f226069f0cb999cfcbcdee393b
apoorvrastogi <apoorvrastogi786@gmail.com>
2023-08-09 00:35:23 +0530

    Resolve index out of bound exception

--- Scaler/src/SmallestPositiveNotPresentInArray.java
+++ Scaler/src/SmallestPositiveNotPresentInArray.java
@@ -8,24 +8,34 @@ public class SmallestPositiveNotPresentInArray {
     private static int smallestMissingPositiveNumber(int[] arr) {
         int n = arr.length;
         boolean isOneAvailable = false;
+
+        // Check if 1 is present in array or not
         for(int num : arr){
             if(num == 1){
                 isOneAvailable = true;
                 break;
             }
         }
+
+        // If 1 is not present
         if(!isOneAvailable)
             return 1;
+
+        // Changing values to 1
         for(int i=0;i<arr.length;i++){
-            if(arr[i] < 0 || arr[i] > n){
+            if(arr[i] <= 0 || arr[i] > n){
                 arr[i] = 1;
             }
         }
+
+        // Updating indices according to values
         for(int i=0;i<n;i++){
-            arr[arr[i] - 1] = arr[i] + n;
+            arr[(arr[i] - 1) % n] +=  n;
         }
+
+        // Finding which index has value less than n
         for(int i=0;i<n;i++){
-            if(arr[i] > 0)
+            if(arr[i] <=n)
                 return i + 1;
         }
         return n + 1;
